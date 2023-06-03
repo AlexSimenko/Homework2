c4b0733569996992c0edf94bb326bf3cfea6140f
AlexSimenko <simenkoalex2@gmail.com>
2023-06-03 20:14:53 +0300

    My commit to my test Work

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
         System.out.println("test github");
         System.out.println("test Simenko_Git");
+        Employeer employeer = new Employeer();
+        Employeer fireMan = new Employeer("chief firefighter","chief@Gmail.com",
+                "380949459495",45,20000);
+
     }
 }
